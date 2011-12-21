0c06e32cc2daa1c421dd22afd9585d3cea50a11b
kandasoft <kandasoft@gmail.com>
2011-12-21 21:17:25 +0400

    test commit

--- string_class/main.cpp
+++ string_class/main.cpp
@@ -3,7 +3,6 @@
 //  string_class
 //
 //  Created by Yumitsu on 21.12.11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
 #include <iostream>
@@ -13,9 +12,5 @@ int main (int argc, const char * argv[])
 
     // insert code here...
     std::cout << "Hello, World!\n";
-    return 0;//
-    //
-    return 0;//
+    return 0;
 }
-
-////
