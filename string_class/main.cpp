fe404a8d3e55ef13128da154d6303daf5c40199b
kandasoft <kandasoft@gmail.com>
2011-12-21 21:12:43 +0400

    new project

---
+++ string_class/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  string_class
+//
+//  Created by Yumitsu on 21.12.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;//
+    //
+    return 0;//
+}
+
+////
