1fb5488790a7513422356f8f30abbeca8aa30b4d
kandasoft <kandasoft@gmail.com>
2011-12-27 04:25:50 +0400

    project

--- string_class/main.cpp
+++ string_class/main.cpp
@@ -6,10 +6,10 @@
 //
 
 #include <iostream>
+#include <stdio.h>
 
 int main (int argc, const char * argv[])
 {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;// 
 }
