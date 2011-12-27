08958dbbf544f8cd1e3bfaa8fa1c69501a50f7f4
kandasoft <kandasoft@gmail.com>
2011-12-27 04:24:15 +0400

    basic class cpp

--- string_class/strcls.cpp
+++ string_class/strcls.cpp
@@ -5,26 +5,63 @@
 //  Created by Kandsoft on 21.12.11.
 
 #include <iostream>
+#include "strcls.h"
+using namespace std;
 
-class Strings {
-private:
-    char *str;
-    int length;
-    
-public:
-    
-    //first constructor without param's
-    Strings(){
-        
-        this->str = new char(1);
-        this->str = "\0";
-        this->length = 0;
+
+/********************** 
+  реализация класса
+***********************/
+
+// пустой конструктор
+Strings::Strings(){
+    rep = new Srep(0,"");
+}
+
+Strings::Strings(const char* s){
+    rep = new Srep(strlen(s),s);
+}
+
+// копирование
+Strings::Strings(const Strings& a){
+    a.rep->n++;
+    rep = a.rep; // разделяемое представление
+}
+   
+Strings::~Strings(){
+    if( --rep->n == 0 ) delete rep;
+}
+
+
+// перегрузка операторов =
+
+Strings& Strings::operator=(const char* s){
+    if (rep->n == 1)
+        rep->assign(strlen(s), s);
+    else {
+        rep->n--;
+        rep = new Srep(strlen(s),s);
     }
     
-    // int to string constructor
-    Strings( int param ){
-        
-        
-    }
-    
-};
+    return *this;
+}
+
+Strings& Strings::operator=(const Strings& x){
+    x.rep->n++;     
+    if( --rep->n == 0) delete rep;
+    rep = x.rep;
+    return *this;
+}
+
+ostream& operator<< (ostream& strm, Strings& str){
+    return strm << str.rep->s << "\0\n";
+}
+
+
+
+
+
+
+
+
+
